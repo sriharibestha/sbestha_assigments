5ad8a8f8e719a72fb34526c0098dfcb2489e48de
Srihari Bestha <sri hari bestha>
2022-07-26 18:00:54 +0530

    programs

--- matrixtranspose.c
+++ matrixtranspose.c
@@ -2,7 +2,31 @@
 #include<stdio.h>
 void main()
 {
-	int a[3][3],row,col,i,j;
-	row=sizeof(a)/sizeof(a[0]);
-	col=sizeof(a[0])/sizeof(a[0][0]);
-	printf("enter 
+	int a[10][10],t[10][10],r,c,i,j;
+	printf("enter the rows and colomns\n");
+	scanf("%d%d",&r,&c);
+	printf("enter the matrix elements\n");
+	for(i=0;i<r;i++)
+		for(j=0;j<c;j++)
+			scanf("%d",&a[i][j]);
+        printf("printing the matrix elements\n");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			printf("%d ",a[i][j]);
+		}
+		printf("\n");
+	}
+	printf("printing the transpose matrix\n");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			printf("%d ",a[j][i]);
+		}
+		printf("\n");
+	}
+
+}
+
